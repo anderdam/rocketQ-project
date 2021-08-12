74810dd619b4667ebfd089a14c5fa1bc6d14a329
AndersonD <anderdam@gmail.com>
2021-08-12 20:07:16 -0300

    Inicialização do node, e inclusão de rotas

---
+++ src/route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const route = express.Router();
+
+route.get('/', (req, res) => res.render('index'));
+route.get('/room', (req, res) => res.render('room'));
+route.get('/create-pass', (req, res) => res.render('create-pass'));
+
+// Formato que o formulário de dentro do modal tem que passar a informação
+// route.post('/room/:room/:question/:action', (req, res) => res.render());
+
+module.exports = route;
